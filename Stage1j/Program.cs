68067f5be38c0d871c86eca8e36bc783b90188c4
Godand <aezeigwe2003@gmail.com>
2024-07-01 19:58:08 -0400

    use .net6

---
+++ Stage1j/Program.cs
@@ -0,0 +1,70 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Stage1.Models;
+using Stage1.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<HttpService>(x=>new HttpService(new HttpClient(),builder.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateTime.Now.AddDays(index),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapGet("/api/hello",async (HttpService client,HttpContext context,[FromQuery]string visitor_name)=>
+{
+    var forwardedFor = context.Request.Headers["X-Forwarded-For"].FirstOrDefault();
+    var clientIp=forwardedFor?.Split(',').FirstOrDefault()?.Trim();
+    if (string.IsNullOrEmpty(clientIp))
+        clientIp = context.Connection.RemoteIpAddress?.ToString();
+    if (clientIp is null)
+        return Results.NotFound("Client Ip Not Found");
+    // api check
+    
+    (string? city,string? temperature)=await client.GetIpDetails(clientIp);
+    if ((city is null) || (temperature is null))
+        return Results.NotFound("Ip Details Was Not Found");
+    
+
+    var response = new ResponseDto(clientIp, city, $"Hello, {visitor_name??"Anonymous"} the temperature is{temperature} degrees Celcius in {city}");
+
+    return Results.Ok(response);
+}).WithName("GetIpInfo");
+
+app.Run();
+
+record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ Stage1j/Services/HttpService.cs
@@ -0,0 +1,72 @@
+using System.Text;
+using System.Text.Json;
+
+
+namespace Stage1.Services
+{
+    public class HttpService
+    {
+        public HttpService(HttpClient client, IConfiguration config)
+        {
+            _client=client;
+            _config=config;
+        }
+        private readonly HttpClient _client;
+        private readonly IConfiguration _config;
+        private readonly string _locationBaseUrl = "http://ip-api.com/json";
+        private readonly string _weatherBaseUrl = "https://api.weatherapi.com/v1/current.json?";
+
+
+        public async Task<(string?, string?)> GetIpDetails(string clientIp)
+        {
+            var locationData = await GetLocation(clientIp);
+            if (locationData is null) return (null,null);
+            Console.WriteLine(locationData);
+
+            var city = locationData["city"]!;
+
+            var weatherData = await GetWeatherDetails(locationData["lon"], locationData["lat"]);
+            if (weatherData is null) return (null,null);
+
+            var temperature = weatherData!?.current?.temp_c;
+            return (city, temperature);
+        }
+        private async Task<Dictionary<string, string>?> GetLocation(string clientIp)
+        {
+            string url = _locationBaseUrl + $"/{clientIp}";
+            var response = await _client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                
+                var content = await response.Content.ReadAsStringAsync();
+                
+                var contentObject =JsonSerializer.Deserialize<Dictionary<string, string>>(content)!;
+                if (contentObject["status"]=="fail"){
+                    return null;
+                }
+                return contentObject;
+            }
+            return null;
+        }
+
+        private async Task<dynamic?> GetWeatherDetails(string longitude, string latitude)
+        {
+            StringBuilder builder = new StringBuilder(_weatherBaseUrl);
+            string key = _config.GetSection("WeatherApiKey").Value!;
+            string[] queryParams = {$"q={latitude}", $"{longitude}", $"&key={key}"};
+            builder.AppendJoin(null, queryParams);
+
+            var response = await _client.GetAsync(builder.ToString());
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStream();
+                return JsonSerializer.Deserialize<dynamic>(content);
+
+            }
+            return null;
+        }
+    }
+
+
+}
